d6e387d68d052e3e8db05a60a904c5f2093897b0
Paulo Pinheiro <pinus.sc@gmail.com>
2014-06-07 15:35:37 -0300

    Start JWorkbookODS class, with ODFDOM dependency (exclude OpenOffice.org jars)

--- jworkbook/src/main/java/br/com/paulopinheiro/jworkbook/factory/JWorkbookODS.java
+++ jworkbook/src/main/java/br/com/paulopinheiro/jworkbook/factory/JWorkbookODS.java
@@ -6,19 +6,26 @@ package br.com.paulopinheiro.jworkbook.factory;
 
 import java.io.File;
 import java.io.IOException;
+import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
+import org.odftoolkit.odfdom.pkg.OdfFileDom;
 
 /**
  *
- * @author usuario
+ * @author Paulo Pinheiro
  */
 class JWorkbookODS implements JWorkbook {
+    private OdfSpreadsheetDocument document;
+    private OdfFileDom dom;
+    private File workbookFile;
+    
     protected JWorkbookODS(File workbookFile) {
-        
-    }
-
-    @Override
-    public void write() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.setWorkbookFile(workbookFile);
+        try {
+            this.setDocument(OdfSpreadsheetDocument.newSpreadsheetDocument());
+            this.setDom(this.getDocument().getContentDom());
+        } catch (Exception ex) {
+            throw new RuntimeException(MessagesBundle.getExceptionMessage("ods.creationError",ex.getMessage()));
+        }
     }
 
     @Override
@@ -40,5 +47,55 @@ class JWorkbookODS implements JWorkbook {
     public void addRow(Object[] cells, boolean tittleTotal) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    @Override
+    public void write() throws IOException {
+        try {
+            this.getDocument().save(this.getWorkbookFile());
+        } catch (Exception ex) {
+            throw new IOException(ex);
+        }
+    }
+
+    /**
+     * @return the document
+     */
+    private OdfSpreadsheetDocument getDocument() {
+        return document;
+    }
+
+    /**
+     * @param document the document to set
+     */
+    private void setDocument(OdfSpreadsheetDocument document) {
+        this.document = document;
+    }
+
+    /**
+     * @return the dom
+     */
+    private OdfFileDom getDom() {
+        return dom;
+    }
+
+    /**
+     * @param dom the dom to set
+     */
+    private void setDom(OdfFileDom dom) {
+        this.dom = dom;
+    }
+
+    /**
+     * @return the workbookFile
+     */
+    private File getWorkbookFile() {
+        return workbookFile;
+    }
+
+    /**
+     * @param workbookFile the workbookFile to set
+     */
+    private void setWorkbookFile(File workbookFile) {
+        this.workbookFile = workbookFile;
+    }
 }
